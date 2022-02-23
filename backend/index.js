5f29dc10d0e34dca01e18212ff24188fec016b8c
Supantha Paul <supapaul56@gmail.com>
2022-02-23 13:53:36 +0530

    added cors in backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 const express = require('express')
+var cors = require('cors')
 require('dotenv').config()
 let mongoose = require('mongoose');
 
 let apiRoutes = require("./api-routes")
 const app = express()
-const port = 3000
+const port = 8000
 
 
 // middlewares
+// app.use(function(req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	next();
+// });
+app.use(cors())
 app.use(express.urlencoded({
 	extended: true
 }));
