f8224ee369adb697682462662f114217781eae15
Ayushi Chaudhuri <61285140+IUC4801@users.noreply.github.com>
2022-02-26 01:18:26 +0530

    Made changes

--- frontend/src/components/pages/Type/Data.js
+++ frontend/src/components/pages/Type/Data.js
@@ -9,7 +9,7 @@ export const homeObjOne = {
     description:
       'Sadness is a normal human emotion, which is generally a reaction to something negative that has happened in your life. However, it subsides after a while. Depression persists.Depression is a mental health concern that severely affects your daily functioning, and has a variety of causes.',
     imgStart: 'start',
-    img: 'images/svg-8.svg',
+    img: 'images/consulting.png',
     alt: 'Vault'
   };
 export const homeObjTwo = {
@@ -33,7 +33,7 @@ export const homeObjTwo = {
     // `,
     // buttonLabel: 'Get Started',
     imgStart: '',
-    img: 'images/laptopDoc.png',
+    img: 'images/boy-sad.png',
     alt: 'Credit Card',
     list: ['Feeling restless, wound-up, or on-edge', 'Being easily fatigued','Having difficulty concentrating; mind going blank','Being irritable','Having muscle tension','Difficulty controlling feelings of worry','Having sleep problems, such as difficulty falling or staying asleep, restlessness, or unsatisfying sleep']
   };
@@ -45,7 +45,7 @@ export const homeObjTwo = {
     headline: 'What causes anxiety?',
     //description:
     imgStart: '',
-    img: 'images/svg-8.svg',
+    img: 'images/guy-angry.png',
     alt: 'Vault',
     list: ['Early adverse life experiences, such as trauma or a history of abuse (for example, child abuse, sexual assault, witnessing violence, etc.)','Experiences related to other ongoing (chronic) medical conditions, such as cancer or diabetes','Biological factors or chemical imbalances in the brain','Having feelings of loneliness or isolation']
   };
@@ -57,7 +57,7 @@ export const homeObjTwo = {
     headline: 'How to cope with anxiety?',
     //description:
     imgStart: '',
-    img: 'images/svg-8.svg',
+    img: 'images/boy-working.png',
     alt: 'Vault',
     list: ['Eating a healthy diet: Diets rich in vegetables, fruits, high-quality meats, fish, nuts, and whole grains can lower the risk of developing anxiety disorders, or help reduce already existing symptoms. But diet alone is probably not enough to treat them.','Consuming probiotics and fermented foods: Taking probiotics and eating fermented foods are helpful in improving overall mental health.','Limiting caffeine: Limiting caffeine would reduce feelings of anxiety and help to control anxiety disorders.', 'Alcohol usage should also be reduced as it affects anxiety levels in an individual.','People suffering from anxiety disorders should be encouraged to quit smoking as it heightens the risk of severe symptoms.','Exercising and practicing yoga can reduce symptoms.']
   };
@@ -78,7 +78,7 @@ export const homeObjTwo = {
     // `,
     //buttonLabel: 'Get Started',
     imgStart: '',
-    img: 'images/careDoc.png',
+    img: 'images/girl-angry.png',
     alt: 'Vault',
     list: ['Re-experiencing the traumatic event through intrusive memories, flashbacks, nightmares, or intense mental or physical reactions when reminded of the trauma.','Avoidance and numbing, such as avoiding anything that reminds one of the trauma, a loss of interest in activities and life in general, feeling emotionally numb and detached from others and a sense of a limited future.','Hyperarousal, including sleep problems, irritability, feeling jumpy or easily startled, angry outbursts, and aggressive, self-destructive, or reckless behaviour.','Negative thought and mood changes like feeling alienated and alone, difficulty concentrating or remembering, depression and hopelessness, feeling mistrust and betrayal, and feeling guilt, shame, or self-blame.']
   };
@@ -90,7 +90,7 @@ export const homeObjTwo = {
     headline: 'What causes PTSD?',
     //description:
     imgStart: '',
-    img: 'images/svg-8.svg',
+    img: 'images/girl-standing-sad.png',
     alt: 'Vault',
     list: ['Serious accidents','Physical or sexual assault abuse, including childhood or domestic abuse','Exposure to traumatic events at work, including remote exposure','Serious health problems, such as being admitted to intensive care','Childbirth experiences, such as losing a baby','War and conflict','Torture']
   };
@@ -102,7 +102,7 @@ export const homeObjTwo = {
     headline: 'How to cope with PTSD?',
     //description:
     imgStart: '',
-    img: 'images/svg-8.svg',
+    img: 'images/man-calm.png',
     alt: 'Vault',
     list: ['Learning more about trauma and PTSD.','Joining a PTSD or social support group.','Expressive writing and grounding.','Practising relaxation techniques and mindfulness.','Pursuing outdoor activities and an overall healthy lifestyle.','Confiding in a person one trusts and reaching out for support.','Spending time with positive people and environment.','Not indulging and taking up negative support of alcohol and drugs.','Taking up therapy or medication if the self-help tips aren’t working effectively. Some of the few examples are Trauma-focused cognitive-behavioural therapy, Family therapy ,and EMDR (Eye Movement Desensitisation and Reprocessing).']
   };
@@ -123,7 +123,7 @@ export const homeObjTwo = {
     // `,
     //buttonLabel: 'Get Started',
     imgStart: '',
-    img: 'images/careDoc.png',
+    img: 'images/redhead-sad.png',
     alt: 'Vault',
     list: ['Medical conditions, such as stroke or cancer Genes', 'People who have a family history of depression may be at higher risk', 'Stress, including caregiver stress', 'Sleep problems', 'Social isolation and loneliness', 'Lack of exercise or physical activity', 'Functional limitations that make engaging in activities of daily living difficult', 'Addiction and/or alcoholism —included in Substance-Induced Depressive Disorder']
   };
@@ -135,7 +135,7 @@ export const homeObjTwo = {
     headline: 'What causes Eating Disorders?',
     //description:
     imgStart: '',
-    img: 'images/svg-8.svg',
+    img: 'images/redhead-laptop.png',
     alt: 'Vault',
     list: ['Early adverse life experiences, such as trauma or a history of abuse (for example, child abuse, sexual assault, witnessing violence, etc.)','Experiences related to other ongoing (chronic) medical conditions, such as cancer or diabetes','Biological factors or chemical imbalances in the brain','Having feelings of loneliness or isolation']
   };
@@ -144,7 +144,7 @@ export const homeObjTwo = {
     lightText: false,
     lightTextDesc: false,
     //topLine: 'Lorem ipsum dolor sit amet',
-    headline: 'What causes depression?',
+    headline: 'How to cope with PTSD?',
     // description:`Depression can be caused by Medical conditions, such as stroke or cancer
     // Genes – people who have a family history of depression may be at higher risk
     // Stress, including caregiver stress
@@ -156,7 +156,7 @@ export const homeObjTwo = {
     // `,
     //buttonLabel: 'Get Started',
     imgStart: '',
-    img: 'images/careDoc.png',
+    img: 'images/girl-happy.png',
     alt: 'Vault',
     list: ['Medical conditions, such as stroke or cancer Genes', 'People who have a family history of depression may be at higher risk', 'Stress, including caregiver stress', 'Sleep problems', 'Social isolation and loneliness', 'Lack of exercise or physical activity', 'Functional limitations that make engaging in activities of daily living difficult', 'Addiction and/or alcoholism —included in Substance-Induced Depressive Disorder']
   };
